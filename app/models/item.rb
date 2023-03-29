ec3dd866ea7033ac62c13251bae62d98b72f8e10
shibaei <121368277+shibaei@users.noreply.github.com>
2023-03-30 06:43:18 +0900

    アソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,13 @@
 class Item < ApplicationRecord
+
+  belongs_to :user
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :postage
+  belongs_to :province
+  belongs_to :shipping_day
+  
 end
