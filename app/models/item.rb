363d9a5b4c722eae6bbb5d28693d84da7e9abbf2
shibaei <121368277+shibaei@users.noreply.github.com>
2023-03-31 06:06:59 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,4 +10,16 @@ class Item < ApplicationRecord
   belongs_to :province
   belongs_to :shipping_day
   
+  validates :item_name, :description, :price, presence: true
+
+
+  with_options numericality: { other_than: 1, message: "can't be blank"} do
+    validates :category
+    validates :condition
+    validates :postage
+    validates :province
+    validates :shipping_day
+  end
+
+
 end
