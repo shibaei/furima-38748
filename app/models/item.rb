3217563d803c1ef69727815bc5211cd41fefbe80
shibaei <121368277+shibaei@users.noreply.github.com>
2023-04-01 16:37:48 +0900

    商品情報登録機能

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,15 +10,15 @@ class Item < ApplicationRecord
   belongs_to :province
   belongs_to :shipping_day
   
-  validates :item_name, :description, :price, presence: true
-
+  validates :item_name, :description, :image, presence: true
+  validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
 
   with_options numericality: { other_than: 1, message: "can't be blank"} do
-    validates :category
-    validates :condition
-    validates :postage
-    validates :province
-    validates :shipping_day
+    validates :category_id
+    validates :condition_id
+    validates :postage_id
+    validates :province_id
+    validates :shipping_day_id
   end
 
 
